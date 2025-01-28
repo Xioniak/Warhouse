04d4a20c163523f1df415054c7bcf12c89ef11b1
Piotr Bara <piotr.bara@icloud.com>
2025-01-28 18:28:16 +0100

    MySql connection

--- src/items/Gun.java
+++ src/items/Gun.java
@@ -1,12 +1,20 @@
 package items;
 
 public class Gun {
-    private String name;
-    private String brand;
-    private String caliber;
-    private int magCap;
-    private double price;
+    private final String name;
+    private final String brand;
+    private final String caliber;
+    private final int magCap;
+    private final double price;
 
+    /**
+     * Konstruktor klasy Gun.
+     * @param name nazwa broni
+     * @param brand marka broni
+     * @param caliber kaliber broni
+     * @param magCap pojemność magazynka
+     * @param price cena broni
+     */
     public Gun(String name, String brand, String caliber, int magCap, double price) {
         this.name = name;
         this.brand = brand;
@@ -15,11 +23,11 @@ public class Gun {
         this.price = price;
     }
 
-    public String getName() {return name;}
-    public String getBrand() {return brand;}
-    public String getCaliber() {return caliber;}
-    public int getMagCap() {return magCap;}
-    public double getPrice() {return price;}
+    public String getName() { return name; }
+    public String getBrand() { return brand; }
+    public String getCaliber() { return caliber; }
+    public int getMagCap() { return magCap; }
+    public double getPrice() { return price; }
 
     @Override
     public String toString() {
---
+++ src/items/Utils.java
@@ -0,0 +1,30 @@
+package items;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils {
+  /**
+   * Pobiera dane o pistoletach z bazy danych.
+   * @return lista danych o pistoletach
+   */
+  public static List<String> getPistolsData() {
+    List<String> pistolsData = new ArrayList<>();
+    try {
+      Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Warhouse", "root", "");
+
+      Statement statement = conn.createStatement();
+      ResultSet resultSet = statement.executeQuery("SELECT * FROM pistol");
+
+      while (resultSet.next()) {
+        String name = resultSet.getString("name");
+        boolean hasSilencer = resultSet.getBoolean("hasSilencer");
+        pistolsData.add(name + " " + hasSilencer);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return pistolsData;
+  }
+}
