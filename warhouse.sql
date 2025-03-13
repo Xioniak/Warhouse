e8741323315150a138a9d3cee7f2848ffe142f6f
PiterB <149414009+Piter-Arab@users.noreply.github.com>
2025-03-13 18:29:30 +0100

    Update warhouse.sql

--- warhouse.sql
+++ warhouse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sty 28, 2025 at 06:33 PM
+-- Generation Time: Mar 13, 2025 at 06:29 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -41,6 +41,47 @@ INSERT INTO `pistol` (`id`, `name`, `hasSilencer`) VALUES
 (1, 'usp', 1),
 (2, 'glock18', 0);
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(20) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `pass_hash` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `login`, `pass_hash`) VALUES
+(2, 'root', '36a9e7f1c95b82ffb99743e0c5c4ce95d83c9a430aac59f84ef3cbfab6145068'),
+(3, 'test', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user_transactions`
+--
+
+CREATE TABLE `user_transactions` (
+  `id` int(20) NOT NULL,
+  `user_id` int(20) NOT NULL,
+  `payment_method` varchar(40) NOT NULL,
+  `price` float NOT NULL,
+  `order_date` date DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_transactions`
+--
+
+INSERT INTO `user_transactions` (`id`, `user_id`, `payment_method`, `price`, `order_date`) VALUES
+(2, 2, 'blik', 549.99, '2025-03-13');
+
 --
 -- Indeksy dla zrzutów tabel
 --
@@ -52,6 +93,19 @@ ALTER TABLE `pistol`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `user_transactions`
+--
+ALTER TABLE `user_transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +114,28 @@ ALTER TABLE `pistol`
 --
 ALTER TABLE `pistol`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user_transactions`
+--
+ALTER TABLE `user_transactions`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_transactions`
+--
+ALTER TABLE `user_transactions`
+  ADD CONSTRAINT `user_transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
